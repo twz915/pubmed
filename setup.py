48cea8e2340e28bef95f8dd52a566ea24b4a17e4
WeizhongTu <tuweizhong@163.com>
2015-01-15 17:28:30 +0800

    modify setup.cfg

--- setup.py
+++ setup.py
@@ -7,8 +7,18 @@ setup(name='pubmed',
       version=version,
       description="Get literature information via PubMed ID",
       long_description="""\
-Get literature information via PubMed ID, such as the author, title, journal_name, pub_date and so on.""",
-      classifiers=[], 
+Get literature information via PubMed ID, such as the author, title, journal_name, pub_date and so on. see https://github.com/twz915/pubmed""",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+
+          'Intended Audience :: Science/Research',
+          # Pick your license as you wish (should match "license" above)
+           'License :: OSI Approved :: MIT License',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 2.7',
+      ], 
       keywords='pubmed literature reference PMID',
       author='Weizhong Tu',
       author_email='mail@tuweizhong.com',
